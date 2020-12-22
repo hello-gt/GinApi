4a8bd0f1d96916c88050290df98e89fcd0940924
hello_lgt <hello_lgt@163.com>
2020-12-23 00:06:36 +0800

    123

--- test.go
+++ test.go
@@ -1,8 +1,14 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	a := 1
+	fmt.Println(a)
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
